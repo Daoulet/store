1aabfa414228d9e901cfd2c0e680d7c7ef2ac39b
Daulet Bimaganbetov <bimaganbetov1995@gmail.com>
2020-04-20 04:56:26 +0600

    sec

--- store_project/settings.py
+++ store_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9dze554$8jl89l_@5d1)b(84ebm07j6r=gs86%6z1*2uzsfu3f'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
