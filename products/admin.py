4e3aab33fdd0a58ccc1a26778834058c0f1e14ff
Daulet Bimaganbetov <bimaganbetov1995@gmail.com>
2020-04-21 04:22:38 +0600

    review

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Review
+
+class ReviewInline(admin.TabularInline):
+    model = Review
+
 
 class ProductAdmin(admin.ModelAdmin):
+	onlines = [
+		ReviewInline,
+	]
 	list_display = ("title", "author", "price",)
 
 admin.site.register(Product, ProductAdmin)
+
+admin.site.register(Review)
--- products/models.py
+++ products/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 class Product(models.Model):
 	title = models.CharField(max_length=200)
@@ -7,3 +9,23 @@ class Product(models.Model):
 
 	def __str__(self):
 		return self.title
+
+	def get_absolute_url(self):
+		return reverse('product_detail', args=[str(self.id)])
+
+
+class Review(models.Model):
+	product = models.ForeignKey(
+		Product,
+		on_delete=models.CASCADE,
+		related_name='reviews',
+	)
+	review = models.CharField(max_length=200)
+	author = models.ForeignKey(
+		get_user_model(),
+		on_delete=models.CASCADE,
+	)
+
+
+	def __str__(self):
+		return self.review
