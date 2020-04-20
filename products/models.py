d2470fb100f04a728ecc5f5d493bacc1cb6676b1
Daulet Bimaganbetov <bimaganbetov1995@gmail.com>
2020-04-21 04:55:07 +0600

    media

--- products/models.py
+++ products/models.py
@@ -6,6 +6,7 @@ class Product(models.Model):
 	title = models.CharField(max_length=200)
 	author = models.CharField(max_length=200)
 	price = models.DecimalField(max_digits=8, decimal_places=2)
+	cover = models.ImageField(upload_to='covers/', blank=True)
 
 	def __str__(self):
 		return self.title
--- store_project/settings.py
+++ store_project/settings.py
@@ -169,3 +169,6 @@ ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
 
 DEFAULT_FROM_EMAIL = 'admin@djangostore.com'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
