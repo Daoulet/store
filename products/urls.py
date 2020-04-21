48c5bc55afe018311198193dbf12538e9989ba0e
Daulet Bimaganbetov <bimaganbetov1995@gmail.com>
2020-04-21 20:59:17 +0600

    Search

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProductListView, ProductDetailView
+from .views import ProductListView, ProductDetailView, SearchResultsListView
 
 urlpatterns = [
 	path('', ProductListView.as_view(), name='product_list'),
+	path('search/', SearchResultsListView.as_view(), name='search_results'),
 	path('<int:pk>', ProductDetailView.as_view(), name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from .models import Product
+from django.db.models import Q
 
 class ProductListView(LoginRequiredMixin, ListView):
 	model = Product
@@ -14,3 +15,15 @@ class ProductDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView)
 	template_name = 'product/product_detail.html'
 	login_url = 'account_login'
 	permission_required = 'product.special_status'
+
+class SearchResultsListView(ListView):
+	model = Product
+	context_object_name = 'product_list'
+	template_name = 'product/search_results.html'
+
+	def get_queryset(self):
+		query = self.request.GET.get('q')
+		return Product.objects.filter(
+			Q(title__icontains=query) | Q(title__icontains=query)
+			
+		)
